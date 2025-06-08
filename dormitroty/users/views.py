82df7fde891476747074457add27fb76cc89712c
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-08 23:16:54 +0330

    the custom login authentication be set

--- dormitroty/users/views.py
+++ dormitroty/users/views.py
@@ -1,15 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .serializers import UserRegisterSerializer
 from drf_spectacular.utils import extend_schema
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .serializers import CustomTokenObtainPairSerializer, CustomTokenRefreshSerializer
 
+from . import models
 
 class UserRegisterView(APIView):
     """
     documentation for UserRegisterView with spectacular
     """
-
+    permission_classes = [permissions.IsAuthenticated]
     @extend_schema(
         summary="Register a new user",
         request=UserRegisterSerializer,
@@ -24,3 +27,18 @@ class UserRegisterView(APIView):
             serializer.save()
             return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """
+        get all users from database
+        """
+        users = models.User.objects.all()
+        serializer = UserRegisterSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class CustomTokenRefreshView(TokenRefreshView):
+    serializer_class = CustomTokenRefreshSerializer
