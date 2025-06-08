fc4904d99a611d8e0065aaa836270b6c26894c2c
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-08 22:55:01 +0330

    added user register post api to docs

--- dormitroty/users/views.py
+++ dormitroty/users/views.py
@@ -2,8 +2,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegisterSerializer
+from drf_spectacular.utils import extend_schema
+
 
 class UserRegisterView(APIView):
+    """
+    documentation for UserRegisterView with spectacular
+    """
+
+    @extend_schema(
+        summary="Register a new user",
+        request=UserRegisterSerializer,
+        responses={
+            201: {'description': 'User created successfully'},
+            400: {'description': 'Invalid data'}
+        }
+    )
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
