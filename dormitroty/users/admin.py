c6cdacb9ab151a9cf2aca60dd5330e6da46b73d7
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-08 21:14:38 +0330

    the user admin be set

--- dormitroty/users/admin.py
+++ dormitroty/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('student_code', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    search_fields = ('student_code', 'email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff', 'gender')
