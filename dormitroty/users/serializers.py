060d8fe105fefcc2c5b52813d0fd4ac111f2c168
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-08 21:18:33 +0330

    the user serializers be set

---
+++ dormitroty/users/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['student_code', 'national_code', 'phone_number', 'gender', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
