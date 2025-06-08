0bc0c4e21468089e02a64c3add6f060de27b7774
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-08 23:16:22 +0330

    added custom serilizer for jwt token

--- dormitroty/users/serializers.py
+++ dormitroty/users/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+
 from .models import User
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -14,3 +16,18 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        # Add custom fields to the response
+        data['student_code'] = self.user.student_code
+        data['password'] = self.user.password
+        return data
+
+class CustomTokenRefreshSerializer(TokenRefreshSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        # Add custom fields to the response if needed
+        return data
