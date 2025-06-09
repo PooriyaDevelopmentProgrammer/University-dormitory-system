2bd6400e23a409e1c1f5779e227b7beade9c7ea1
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-09 23:52:31 +0330

    added logout url

--- dormitroty/users/urls.py
+++ dormitroty/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, LogoutView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-register'),
     path('token/', TokenObtainPairView.as_view(), name='token-obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
