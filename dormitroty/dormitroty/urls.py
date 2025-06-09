fa874ea6b20ebfe42795d402f7016d48883b2bfb
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-10 00:28:12 +0330

    the user base urls be updated

--- dormitroty/dormitroty/urls.py
+++ dormitroty/dormitroty/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('api/users/', include('users.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
