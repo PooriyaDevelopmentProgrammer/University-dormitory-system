449d5fea410dfc571633000c51eb60d7ed49ec31
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-10 00:10:21 +0330

    the database test config be added

--- dormitroty/dormitroty/settings.py
+++ dormitroty/dormitroty/settings.py
@@ -107,6 +107,14 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': os.environ.get('POSTGRES_PORT'),
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_TEST_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
