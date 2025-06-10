44eeda5a221f10d7b69b4544d7bffdc9936f1fea
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-10 23:52:51 +0330

    the dorms apis urls be added

---
+++ dormitroty/dorms/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from dorms.views import DormAPIView, RoomAPIView, BedAPIView
+
+urlpatterns = [
+    path('', DormAPIView.as_view(), name='dorm-list'),
+    path('rooms/', RoomAPIView.as_view(), name='room-list'),
+    path('beds/', BedAPIView.as_view(), name='bed-list'),
+]
