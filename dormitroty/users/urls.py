9b4004b8d72dbb8a2b9e69a777673fa486e39fb7
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-10 03:48:03 +0330

    the user details urls be set

--- dormitroty/users/urls.py
+++ dormitroty/users/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView, LogoutView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
+from .views import UserRegisterView, LogoutView, UserDetailView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('', UserRegisterView.as_view(), name='user-register'),
+    path('details/<str:user_id>/', UserDetailView.as_view(), name='user-details'),
     path('token/', TokenObtainPairView.as_view(), name='token-obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
