c8d61d06f30fe7a2c5ed7e745f13a0089b9195a8
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-10 23:50:41 +0330

    the dorms app urls be added to base

--- dormitroty/dormitroty/urls.py
+++ dormitroty/dormitroty/urls.py
@@ -21,6 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/dorms/', include('dorms.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
