8d0008e4c342174557329c9a551ef4c174fdd30e
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-06-12 01:50:05 +0330

    the dorms details url for api be set

--- dormitroty/dorms/urls.py
+++ dormitroty/dorms/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from dorms.views import DormAPIView, RoomAPIView, BedAPIView
+from dorms.views import DormAPIView, RoomAPIView, BedAPIView, DormDetailsAPIView
 
 urlpatterns = [
     path('', DormAPIView.as_view(), name='dorm-list'),
+    path('details/<int:pk>/', DormDetailsAPIView.as_view(), name='dorm-list'),
     path('rooms/', RoomAPIView.as_view(), name='room-list'),
     path('beds/', BedAPIView.as_view(), name='bed-list'),
 ]
