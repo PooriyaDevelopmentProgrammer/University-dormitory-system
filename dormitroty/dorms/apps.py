645d46258210abed194e7032e70549835fe5fa27
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-07-15 02:17:19 +0330

    the dorms apps file be updated

--- dormitroty/dorms/apps.py
+++ dormitroty/dorms/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DormsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dorms'
+
+    def ready(self):
+        import dorms.signals
