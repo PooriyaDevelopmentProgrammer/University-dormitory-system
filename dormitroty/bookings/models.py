677b8c16c940b35411e605cbd53264fd709306a0
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-07-15 02:15:31 +0330

    the booking models be modified

--- dormitroty/bookings/models.py
+++ dormitroty/bookings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
-from dorms.models import Room
+from dorms.models import Room, Bed
+
 
 class Booking(models.Model):
     class BookingStatus(models.TextChoices):
@@ -11,14 +12,18 @@ class Booking(models.Model):
 
     student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True, blank=True)
+    bed = models.ForeignKey(Bed, on_delete=models.SET_NULL, null=True, blank=True)
     status = models.CharField(max_length=10, choices=BookingStatus.choices, default=BookingStatus.PENDING)
     start_date = models.DateField()
     end_date = models.DateField()
+    rejection_reason = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Booking by {self.student.student_code} ({self.status})"
 
+
+"""
 class BookingHistory(models.Model):
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='history')
     status = models.CharField(max_length=10, choices=Booking.BookingStatus.choices)
@@ -27,3 +32,4 @@ class BookingHistory(models.Model):
 
     def __str__(self):
         return f"{self.booking} changed to {self.status}"
+"""
