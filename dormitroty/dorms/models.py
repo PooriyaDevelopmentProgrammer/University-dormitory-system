a8a04ee97df17b437eae6543ae8a4a5311003ebb
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-07-15 02:18:19 +0330

    the rooms model be updated

--- dormitroty/dorms/models.py
+++ dormitroty/dorms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Dorm(models.Model):
     name = models.CharField(max_length=100)
     location = models.TextField()
@@ -19,12 +20,21 @@ class Room(models.Model):
     room_number = models.CharField(max_length=10)
     capacity = models.PositiveIntegerField()
     floor = models.IntegerField()
+    full = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Room {self.room_number} - {self.dorm.name}"
 
     def available_beds(self):
         return self.beds.filter(is_occupied=False).count()
+
+    def set_full_true(self):
+        if self.available_beds() == 0:
+            self.full = True
+            self.save()
+        else:
+            self.full = False
+            self.save()
 
 
 class Bed(models.Model):
