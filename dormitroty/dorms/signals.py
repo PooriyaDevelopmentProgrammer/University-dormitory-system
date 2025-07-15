967cf1428d068b23f5c1947cb84f70646955bede
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-07-15 04:02:01 +0330

    dorms signals file be modified

--- dormitroty/dorms/signals.py
+++ dormitroty/dorms/signals.py
@@ -1,9 +1,13 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Bed
+from .models import Bed , Room
 
 @receiver(post_save, sender=Bed)
 @receiver(post_delete, sender=Bed)
 def update_room_full_status(sender, instance, **kwargs):
     room = instance.room
     room.set_full_true()
+
+@receiver(post_delete, sender=Bed)
+def resequence_beds(sender, instance, **kwargs):
+    Room.resequence_beds_for_room(instance.room)
