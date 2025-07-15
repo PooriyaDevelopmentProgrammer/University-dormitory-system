7de849cbe985ad88e3419dd48d0b8d367bf29e72
PooriyaDevelopmentProgrammer <pooriyakarimshamsi@gmail.com>
2025-07-16 03:20:27 +0330

    the booking details url be created

--- dormitroty/bookings/urls.py
+++ dormitroty/bookings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookingListCreateAPIView
+from .views import BookingListCreateAPIView, BookingDetailAPIView
 
 urlpatterns = [
     path('', BookingListCreateAPIView.as_view(), name='booking-list-create'),
+    path('details/<str:booking_id>/', BookingDetailAPIView.as_view(), name='booking-list-create'),
 ]
