34195617b11fa21cbbcbaada50589bb8b9525fe5
pooriya <pooriya.adib.engineer@gmail.com>
2025-07-20 01:30:12 +0330

    the complaints models be created

--- dormitroty/complaints/models.py
+++ dormitroty/complaints/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django_jalali.db import models as jalali_models
 
-# Create your models here.
+class Complaint(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = jalali_models.jDateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.student.student_code}"
+
+
+class ComplaintMessage(models.Model):
+    complaint = models.ForeignKey(Complaint, related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = jalali_models.jDateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Msg by {self.sender.student_code} on {self.created_at.strftime('%Y-%m-%d')}"
